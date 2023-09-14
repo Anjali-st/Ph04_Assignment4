4b0b1e73a139998cff66dbe407ae34e5197abbd1
Anjali-st <anjali90908080@gmail.com>
2023-09-14 22:37:57 +0530

    WebAppAssignment_04

---
+++ Data/Assignment_04DbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebAppAssignment_04.Models;
+
+namespace WebAppAssignment_04.Data
+{
+    public class Assignment_04DbContext : DbContext
+    {
+        public Assignment_04DbContext (DbContextOptions<Assignment_04DbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebAppAssignment_04.Models.Student> Student { get; set; } = default!;
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace WebAppAssignment_04.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Class { get; set; }
+        public int Age { get; set; }
+    }
+}
